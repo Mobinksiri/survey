7da809b09741038415cd9bf3a2aa24a0f1ee7830
mobin kasiri <m.kasiri@sibitec.com>
2024-05-11 12:41:03 +0330

    take poll for user completed

--- app/_apiCall/apiCall.ts
+++ app/_apiCall/apiCall.ts
@@ -1,145 +1,153 @@
 import axios, { AxiosResponse } from "axios";
 import { useQuery, UseQueryOptions } from "react-query";
-import { getFromLocalStorage, removeFromLocalStorage } from "../_utils/localStorage";
+import {
+  getFromLocalStorage,
+  removeFromLocalStorage,
+} from "../_utils/localStorage";
 import { store } from "../store";
 import { setApiLoading } from "../store/loading";
 import toast from "react-hot-toast";
 import { NEED_LOGIN, TOKEN } from "../_toast_messages";
 
 interface ApiCallOptions {
-   url: string;
-   params?: any;
-   data?: {};
-   method?: string;
-   callback: (response: any, error: any) => void;
-   formDataIsNeeded?: boolean;
-   loading?: boolean;
-   baseUrl: string;
+  url: string;
+  params?: any;
+  data?: {};
+  method?: string;
+  callback: (response: any, error: any) => void;
+  formDataIsNeeded?: boolean;
+  loading?: boolean;
+  baseUrl: string;
 }
 
 interface useApiCallOptions {
-   url: string; // URL can be dynamic based on the prop
-   params?: any;
-   data?: {};
-   method?: string;
-   shouldCallApi?: boolean;
-   formDataIsNeeded?: boolean;
-   queryOptions?: UseQueryOptions<any, Error>;
-   loading?: boolean;
-   baseUrl: string;
+  url: string; // URL can be dynamic based on the prop
+  params?: any;
+  data?: {};
+  method?: string;
+  shouldCallApi?: boolean;
+  formDataIsNeeded?: boolean;
+  queryOptions?: UseQueryOptions<any, Error>;
+  loading?: boolean;
+  baseUrl: string;
 }
 
 const apiCall = async ({
-   baseUrl,
-   url = "",
-   params = {},
-   data = {},
-   method = "get",
-   callback,
-   formDataIsNeeded = false,
-   loading = true,
+  baseUrl,
+  url = "",
+  params = {},
+  data = {},
+  method = "get",
+  callback,
+  formDataIsNeeded = false,
+  loading = true,
 }: ApiCallOptions): Promise<void> => {
-   const userData: any = getFromLocalStorage("user");
+  const userData: any = getFromLocalStorage("user");
 
-   const cancelTokenSource = axios.CancelToken.source();
+  const cancelTokenSource = axios.CancelToken.source();
 
-   const headers: any = {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-      authorization: "Bearer " + userData?.token,
-   };
+  const headers: any = {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    authorization: "Bearer " + userData?.token,
+  };
 
-   if (formDataIsNeeded) {
-      headers["Content-Type"] = "multipart/form-data";
-   }
+  if (formDataIsNeeded) {
+    headers["Content-Type"] = "multipart/form-data";
+  }
 
-   const completeUrl: string = baseUrl + url;
+  const completeUrl: string = baseUrl + url;
 
-   if (loading) {
-      store.dispatch(setApiLoading(true));
-   }
+  if (loading) {
+    store.dispatch(setApiLoading(true));
+  }
 
-   try {
-      const response: AxiosResponse = await axios({
-         method,
-         url: completeUrl,
-         headers,
-         params,
-         data,
-         cancelToken: cancelTokenSource.token,
-      });
+  try {
+    const response: AxiosResponse = await axios({
+      method,
+      url: completeUrl,
+      headers,
+      params,
+      data,
+      cancelToken: cancelTokenSource.token,
+    });
 
-      callback(response.data, null);
-      if (loading) {
-         store.dispatch(setApiLoading(false));
-      }
-   } catch (error: any) {
-      if (error?.response?.status === 401 || error?.response?.status === 403) {
-         if (userData) {
-            toast.error(TOKEN);
-            if (typeof window !== undefined) {
-               window.location.href = "/";
-               removeFromLocalStorage("user");
-            }
-         } else {
-            toast.error(NEED_LOGIN);
-         }
-         return;
-      }
-
-      callback(null, error?.response?.data);
-      toast?.error(error?.response?.data?.message);
+    callback(response.data, null);
+    if (loading) {
       store.dispatch(setApiLoading(false));
-   }
+    }
+  } catch (error: any) {
+    if (error?.response?.status === 401 || error?.response?.status === 403) {
+      if (userData) {
+        toast.error(TOKEN);
+        if (typeof window !== undefined) {
+          window.location.href = "/";
+          removeFromLocalStorage("user");
+        }
+      } else {
+        toast.error(NEED_LOGIN);
+      }
+      store.dispatch(setApiLoading(false));
+      return;
+    }
+
+    callback(null, error?.response?.data);
+    toast?.error(error?.response?.data?.message);
+    store.dispatch(setApiLoading(false));
+  }
 };
 
 export default apiCall;
 
 export const useApiCall = <T>({
-   url: propUrl,
-   params = {},
-   data: sendedData = {},
-   method = "get",
-   shouldCallApi = true,
-   formDataIsNeeded,
-   queryOptions = {
-      enabled: true,
-      retry: 5,
-      retryDelay: 10000,
-      cacheTime: 100,
-      staleTime: 600000,
-   },
-   loading = true,
-   baseUrl,
+  url: propUrl,
+  params = {},
+  data: sendedData = {},
+  method = "get",
+  shouldCallApi = true,
+  formDataIsNeeded,
+  queryOptions = {
+    enabled: true,
+    retry: 5,
+    retryDelay: 10000,
+    cacheTime: 100,
+    staleTime: 600000,
+  },
+  loading = true,
+  baseUrl,
 }: useApiCallOptions) => {
-   const apiUrl = shouldCallApi ? propUrl : "";
+  const apiUrl = shouldCallApi ? propUrl : "";
 
-   const fetchData = async () => {
-      return new Promise<any>((resolve, reject) => {
-         if (shouldCallApi) {
-            apiCall({
-               baseUrl,
-               url: apiUrl,
-               params,
-               data: sendedData,
-               method,
-               callback: (responseData, error) => {
-                  if (error) {
-                     reject(error);
-                  } else {
-                     resolve(responseData);
-                  }
-               },
-               formDataIsNeeded,
-               loading,
-            });
-         } else {
-            resolve(null);
-         }
-      });
-   };
+  const fetchData = async () => {
+    return new Promise<any>((resolve, reject) => {
+      if (shouldCallApi) {
+        apiCall({
+          baseUrl,
+          url: apiUrl,
+          params,
+          data: sendedData,
+          method,
+          callback: (responseData, error) => {
+            if (error) {
+              reject(error);
+            } else {
+              resolve(responseData);
+            }
+          },
+          formDataIsNeeded,
+          loading,
+        });
+      } else {
+        resolve(null);
+      }
+    });
+  };
 
-   const { data, error, isLoading, refetch } = useQuery<T, Error>(apiUrl, fetchData, queryOptions);
+  const { data, error, isLoading, refetch } = useQuery<T, Error>(
+    apiUrl,
+    fetchData,
+    queryOptions
+  );
 
-   return { data, error, isLoading, refetch };
+  return { data, error, isLoading, refetch };
 };
--- app/store/test.ts
+++ app/store/test.ts
@@ -1,5 +1,5 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { storeInterface, testProps } from '../_interface';
+import { createSlice } from "@reduxjs/toolkit";
+import { storeInterface, testProps } from "../_interface";
 
 const initialState: testProps = {
   indexOfTest: 1,
@@ -8,7 +8,7 @@ const initialState: testProps = {
 };
 
 const test = createSlice({
-  name: 'test',
+  name: "test",
   initialState,
   reducers: {
     setTest: (state, action) => {
@@ -16,31 +16,48 @@ const test = createSlice({
       const questionType = action.payload.questionType;
       const id = action.payload.id;
       const choiceId = action.payload.choiceId;
+      const pollId = action.payload?.pollId;
 
       switch (type) {
-        case 'INC_STEP':
+        case "INC_STEP":
           state.indexOfTest = state.indexOfTest + 1;
           break;
-        case 'DEC_STEP':
+        case "DEC_STEP":
           state.indexOfTest = state.indexOfTest - 1;
           break;
 
-        case 'ACCESS':
+        case "ACCESS":
           state.access = action?.payload?.data;
           break;
 
-        case 'ADD_QUESTION':
-          state.answers = state.answers?.filter((item) => item?.questionId !== id);
+        case "ADD_QUESTION":
+          state.answers = state.answers?.filter(
+            (item) => item?.questionId !== id
+          );
           if (questionType == 1 || questionType == 2) {
             state.answers = [
               ...state.answers,
-              { questionId: id, score: null, choiceId, description: null },
+              {
+                id: 0,
+                questionId: id,
+                score: null,
+                choiceId,
+                description: null,
+                pollId,
+              },
             ];
           }
           if (questionType == 3 || questionType == 4) {
             state.answers = [
               ...state.answers,
-              { questionId: id, score: choiceId, choiceId: null, description: null },
+              {
+                id: 0,
+                questionId: id,
+                score: choiceId,
+                choiceId: null,
+                description: null,
+                pollId,
+              },
             ];
           }
           if (questionType == 5) {
@@ -49,13 +66,20 @@ const test = createSlice({
             } else {
               state.answers = [
                 ...state.answers,
-                { questionId: id, score: null, choiceId: null, description: choiceId },
+                {
+                  id: 0,
+                  questionId: id,
+                  score: null,
+                  choiceId: null,
+                  description: choiceId,
+                  pollId,
+                },
               ];
             }
           }
           break;
 
-        case 'RESET':
+        case "RESET":
           state = {
             indexOfTest: 1,
             answers: [],
